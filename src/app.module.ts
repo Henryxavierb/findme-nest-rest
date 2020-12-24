00fe424e4562f6f5afca027ec08cfd4b8795c0ec
Henrique Barros <henriquexavierb@gmail.com>
2020-12-24 18:38:35 -0300

    chore: set typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import env from 'dotenv';
 import { Module } from '@nestjs/common';
-import { UserController } from './controllers/userController';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './services/user.service';
+import { UserController } from './controllers/userController';
+
+env.config();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      port: 3306,
+      entities: [],
+      type: 'postgres',
+      synchronize: true,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
