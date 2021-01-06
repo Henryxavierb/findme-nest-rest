c775bcc07bf97abcd80f27dfd571970d111beef7
Henrique Barros <henriquexavierb@gmail.com>
2021-01-06 15:56:30 -0300

    refactor: delete unused files
    Defining tests to find users and create new user

---
+++ src/tests/user.controller.spec.ts
@@ -0,0 +1,64 @@
+import TestUser from '../utils/test/test.user';
+import { User } from '../entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from '../providers/user.service';
+
+describe('UserController', () => {
+  let userService: UserService;
+
+  const mockRepository = {
+    find: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
+    findOne: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          useValue: mockRepository,
+          provide: getRepositoryToken(User),
+        },
+      ],
+    }).compile();
+
+    userService = moduleRef.get<UserService>(UserService);
+  });
+
+  describe('findAll', () => {
+    it('should be returned all users', async () => {
+      const user = TestUser.giveValidUser();
+      mockRepository.find.mockReturnValue([user, user]);
+
+      const users = await userService.findAll();
+
+      expect(users).toHaveLength(2);
+      expect(mockRepository.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('create', () => {
+    it('should be created a new user', async () => {
+      const user = TestUser.giveValidUser();
+      mockRepository.save.mockReturnValue(user);
+      mockRepository.create.mockReturnValue(user);
+
+      const newUser = await userService.createUser(user);
+
+      expect(newUser).toMatchObject({
+        id: '123456',
+        name: 'example name',
+        password: '12345678',
+        email: 'example@gmail.com',
+      });
+
+      expect(mockRepository.save).toHaveBeenCalledTimes(1);
+      expect(mockRepository.create).toHaveBeenCalledTimes(1);
+    });
+  });
+});
