6bf45feca57177f2dd26e3a8303f24bd433ceba0
Henrique Barros <henriquexavierb@gmail.com>
2021-01-06 15:56:18 -0300

    refactor: return promise to create user

--- src/providers/user.service.ts
+++ src/providers/user.service.ts
@@ -1,9 +1,9 @@
-import crypto from 'crypto';
+import * as crypto from 'crypto';
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 import { Repository } from 'typeorm';
-import { UserModel } from '../interfaces';
 import { Injectable } from '@nestjs/common';
+import { NewUserSchema } from '../interfaces';
 import { User } from '../entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -16,30 +16,20 @@ export class UserService {
     if (emailAlreadyUsed) throw new Error('O email atual já está em uso');
   }
 
-  validatePassword(password: string) {
-    const MIN_ALLOWED_PASSWORD_LENGTH = 8;
-
-    if (password.length < MIN_ALLOWED_PASSWORD_LENGTH)
-      throw new Error('A senha deve possuir no mínimo 8 caracteres');
-  }
-
   encryptPassword(password: string): string {
     return bcrypt.hashSync(password, 10);
   }
 
-  createUser(userData: UserModel) {
+  createUser(userData: NewUserSchema): Promise<User> {
     const password = this.encryptPassword(userData.password);
     const userId = crypto.randomBytes(5).toString('hex');
     const user = this.userRepo.create({ ...userData, password, id: userId });
 
-    const saveUserInstance = this.userRepo.save(user);
-    const accessToken = this.generateToken({ id: userData.id });
-
-    return { accessToken, data: saveUserInstance };
+    return this.userRepo.save(user);
   }
 
   generateToken(params: Record<string, string>): string {
-    return jwt.sign(params, process.env.SECRET_TO_ACCESS_TOKEN);
+    return jwt.sign(params, process.env.JWT_KEY || '');
   }
 
   findAll(): Promise<User[]> {
