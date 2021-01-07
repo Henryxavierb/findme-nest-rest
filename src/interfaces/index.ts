7dfb72cd640457efc66a313c71ae9a496443d4b8
Henrique Barros <henriquexavierb@gmail.com>
2021-01-06 21:34:23 -0300

    refactor: remove zod typing

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,11 +1,5 @@
-import * as z from 'zod';
-
-export const RequestUserSchema = z.object({
-  name: z.string().nonempty('Nome nao deve ser vazio'),
-  email: z.string().email({ message: 'Email possui formato inválido' }),
-  password: z
-    .string()
-    .min(8, { message: 'Senha deve possui no mínimo 8 caracteres' }),
-});
-
-export type NewUserSchema = z.infer<typeof RequestUserSchema>;
+export type UserSchema = {
+  name: string;
+  email: string;
+  password: string;
+};
